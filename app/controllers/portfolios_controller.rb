b6a5c73973ebc34eb9359be1c0c64a18d1518641
Mustafa Murar <mustafa14@gmail.com>
2021-01-25 21:35:18 +0200

    Integrated edit and update for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -14,10 +14,22 @@ def create
     respond_to do |format|
       if @portfolio_items.save
         format.html { redirect_to portfolios_path notice: 'Portfolio item now Live' }
-        format.json { render :show, status: :created, location: @portfolio_items }
       else
         format.html { render :new }
-        format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+def edit
+	@portfolio_item = Portfolio.find(params[:id])
+end
+
+def update
+    respond_to do |format|
+      if Portfolio.find(params[:id]).update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item Updated' }
+      else
+        format.html { render :edit }
       end
     end
   end
